ab308b612b3571e3cdb9f39fea7a05626e35dd61
Mike Zornek <mike@clickablebliss.com>
2017-02-23 23:33:33 -0500

    new paths

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -7,5 +7,5 @@ set -e
 if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   tar xvzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-  cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
+  cp hugo_${HUGO_VERSION}_Linux-64bit/hugo_${HUGO_VERSION}_Linux-64bit $CIRCLE_BUILD_DIR/bin/hugo
 fi
